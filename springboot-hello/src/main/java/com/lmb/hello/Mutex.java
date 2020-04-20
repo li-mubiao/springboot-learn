e9a1eb1ddafc690e6f2e2dbb448f049d94d0124a
李木标 <limubiao@qipeipu.com>
2020-04-20 21:21:24 +0800

    测试

---
+++ springboot-hello/src/main/java/com/lmb/hello/Mutex.java
@@ -0,0 +1,82 @@
+package com.lmb.hello;
+
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * Copyright (C), 2020,
+ * FileName: Mutex
+ * Author:   mubiao0205@gmail.com
+ * Date:     2020/4/15 21:23
+ * Description: 同步锁的实现
+ * Version:1.0.0
+ */
+public class Mutex implements Lock {
+
+    private static class Sync extends AbstractQueuedSynchronizer {
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        public boolean tryAcquire(int args) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        public boolean tryRelease(int release) {
+            if (getState() == 0) {
+                throw new IllegalMonitorStateException();
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+        Condition newCondition(){
+            return new ConditionObject();
+        }
+    }
+
+    private final Sync sync = new Sync();
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireSharedNanos(1,unit.toNanos(time));
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+    public static void main(String[] args) {
+        Lock lock = new Mutex();
+        System.out.println(lock);
+    }
+}
