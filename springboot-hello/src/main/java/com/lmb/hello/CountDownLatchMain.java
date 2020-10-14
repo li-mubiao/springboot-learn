3d0bfdf670515303d975b69775b7c5b3fe266633
limubiao <limubiao@i72.com>
2020-10-14 16:10:53 +0800

    thread run

--- springboot-hello/src/main/java/com/lmb/hello/CountDownLatchMain.java
+++ springboot-hello/src/main/java/com/lmb/hello/CountDownLatchMain.java
@@ -1,10 +1,13 @@
 package com.lmb.hello;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author 李木标 limubiao@i72.com
@@ -13,7 +16,43 @@ import java.util.concurrent.Executors;
 public class CountDownLatchMain {
 
     public static void main(String[] args) throws InterruptedException {
+//        countDownLatchTest();
+        countDownLatchTest2();
+    }
 
+    private static void countDownLatchTest2() {
+        final CountDownLatch down = new CountDownLatch(10);
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    try {
+
+                        down.await();
+
+                    } catch (Exception e) {
+
+                    }
+
+                    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss|SSS");
+
+                    String orderNo = sdf.format(new Date());
+
+                    System.err.println("生成的订单号是：" + orderNo);
+
+                }
+
+            }).start();
+
+        }
+
+        down.countDown();
+
+    }
+
+
+    private static void countDownLatchTest() throws InterruptedException {
         ExecutorService exector = Executors.newFixedThreadPool(5);
         List<Integer> list = new ArrayList<>();
         list.add(1);
@@ -22,6 +61,7 @@ public class CountDownLatchMain {
         list.add(4);
         int threadNum = list.size();
         final CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+        final AtomicLong atomicLong = new AtomicLong(threadNum);
         for (int i = 0; i < list.size(); i++) {
             final int threadID = i;
             exector.execute(
@@ -29,19 +69,20 @@ public class CountDownLatchMain {
                         try {
                             Thread.sleep(2000);
                             System.out.println(String.format("threadID:[%s] finished!!", threadID));
-                            System.out.println(countDownLatch.getCount());
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         } finally {
-                            countDownLatch.countDown();  //这个不管是否异常都需要数量减,否则会被堵塞无法结束
+                            countDownLatch.countDown();
+                            atomicLong.incrementAndGet();
                         }
                     }
             );
 
         }
+
         countDownLatch.await();//保证之前的所有的线程都执行完成，才会走下面的
         System.out.println(countDownLatch.getCount());
         System.out.println("main thread finished!!");
-
+        System.out.println("atomicLong" + atomicLong);
     }
 }
