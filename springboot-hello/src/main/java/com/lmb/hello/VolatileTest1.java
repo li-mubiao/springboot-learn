5d981a4837c4979bae0c770dbfe3425909706179
李木标 <limubiao@qipeipu.com>
2021-03-11 22:58:57 +0800

    volatile 不能保证原子性示例

---
+++ springboot-hello/src/main/java/com/lmb/hello/VolatileTest1.java
@@ -0,0 +1,52 @@
+package com.lmb.hello;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * Copyright (C), 2021,
+ * FileName: VolatileTest1
+ * Author:   mubiao0205@gmail.com
+ * Date:     2021/3/11 下午10:42
+ * Description: TODO
+ * Version:1.0.0
+ */
+public class VolatileTest1 {
+
+
+
+
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+
+        SynchronizedTest synchronizedTest = new SynchronizedTest();
+            for (int i = 0; i < 1000; i++) {
+                executorService.submit(synchronizedTest);
+            }
+        int size = synchronizedTest.getList().size();
+        System.out.println(size);
+        executorService.shutdown();
+    }
+}
+
+class SynchronizedTest implements Runnable{
+
+
+    volatile int num = 0;
+    volatile List<Integer> list =new ArrayList<>();
+    @Override
+    public synchronized  void run() {
+        num ++;
+        list.add(num);
+    }
+    public List<Integer> getList(){
+        return list;
+    }
+
+}
+
