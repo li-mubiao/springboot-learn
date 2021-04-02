2afefd812108b297afaccd2087359d6d7a68b7b8
limubiao <limubiao@i72.com>
2021-04-02 17:49:57 +0800

    123

---
+++ springboot-hello/src/main/java/com/lmb/hello/juc/Counter1.java
@@ -0,0 +1,38 @@
+package com.lmb.hello.juc;
+
+import java.util.Arrays;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * @author 李木标 limubiao@i72.com
+ * @since 2021-04-02 17:24
+ */
+public class Counter1 {
+
+    private final ReadWriteLock rwlock = new ReentrantReadWriteLock();
+    private final Lock rlock = rwlock.readLock();
+    private final Lock wlock = rwlock.writeLock();
+    private int[] counts = new int[10];
+
+    public void inc(int index) {
+        wlock.lock(); // 加写锁
+        try {
+            counts[index] += 1;
+        } finally {
+            wlock.unlock(); // 释放写锁
+        }
+    }
+
+    public int[] get() {
+        rlock.lock(); // 加读锁
+        try {
+            return Arrays.copyOf(counts, counts.length);
+        } finally {
+            rlock.unlock(); // 释放读锁
+        }
+    }
+
+}
